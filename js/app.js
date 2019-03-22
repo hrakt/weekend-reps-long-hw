47aa9e56da4c4bae1469dcfe37e23445d001630d
hrakt <hraktorres@gmail.com>
2019-03-22 14:41:33 -0700

    Commit 2 - Part B done

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// PART A
+
+// How do we assign a value to a variable?
+// let, const, var followed by the name of the variable and equal it to the value
+
+// How do we change the value of a variable?
+// just name of the variable and make it equal to whatever the new value is going to be
+
+// How do we assign an existing variable to a new variable?
+// ex. let newVar = oldVar;
+
+// Remind me, what are declare, assign, and define?
+// declare is when you declare the variable, so assign name, not necessary to assgin it something
+// assign is when you assign a specific value to the variable
+// define is when you decalare and at the same time define what its going to hold
+
+
+// What is pseudocoding and why should you do it?
+//pseduocoding is when you type out the code in plain english to understand what needs to coded
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+// most of the time you need to spend pseudocoding and understanding the logic behind the problem
+// once you figured out the logic and the pseudocode then you need to start working on the code
+
+// PART B
+
+var firstVariable = "Hello World";
+firstVariable = 12;
+var secondVariable = firstVariable;
+secondVariable = "Bye World";
+console.log(firstVariable); // value of the first variable
+
+var yourName = "Hrak T";
+var newStr = "Hello my name is " + yourName;
+console.log(newStr);
+
+
