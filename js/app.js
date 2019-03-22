01321125d380e78f813b675c9143fed8582f5ed4
hrakt <hraktorres@gmail.com>
2019-03-22 15:19:03 -0700

    Commit 7 - Part 2.B done

--- js/app.js
+++ js/app.js
@@ -90,5 +90,15 @@ for(let i = 12; i<=4000;i+=3){
     console.log(i);
 }
 
+// B. Get even
 
+
+for(let i = 1;i<=100;i++){
+    if(i%2==0){
+        console.log(i + "<-- this is a even number");
+        
+    }else{
+        console.log(i);
+    }
+}
 
