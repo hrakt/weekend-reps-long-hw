96b4c9fb49c7ef8bae8409ef9c662644644ef0bb
hrakt <hraktorres@gmail.com>
2019-03-22 16:34:27 -0700

    Commit 17 - Part 3.H Done

--- js/app.js
+++ js/app.js
@@ -210,3 +210,50 @@ if(int<5){
 }else{
     console.log('monkey');
 }
+
+// H. What's in Your Closet?
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+  console.log("Kristy is rocking that " + kristynsCloset[2] + "today!");
+  kristynsCloset.splice(6,0,'raybans');
+  
+  kristynsCloset[5] = "stained knit hat";
+  
+  console.log(kristynsCloset);
+
+  console.log(thomsCloset[0][0]);
+  console.log(thomsCloset[2][1]);
+  console.log("Thom is looking fierce in a " + thomsCloset[0][0] + " " + thomsCloset[1][1] +  " and " + thomsCloset[2][1]);
+
+  thomsCloset[1][2] = 'Footie Pajamas';
+  
