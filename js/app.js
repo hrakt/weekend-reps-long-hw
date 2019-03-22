9e9425d4b80c9250db3a05da24d8fc9c990e66d4
hrakt <hraktorres@gmail.com>
2019-03-22 15:24:06 -0700

    Commit 8 - Part 2.C done

--- js/app.js
+++ js/app.js
@@ -1,104 +1,116 @@
-// // PART A
+// // // PART A
 
-// // How do we assign a value to a variable?
-// // let, const, var followed by the name of the variable and equal it to the value
+// // // How do we assign a value to a variable?
+// // // let, const, var followed by the name of the variable and equal it to the value
 
-// // How do we change the value of a variable?
-// // just name of the variable and make it equal to whatever the new value is going to be
+// // // How do we change the value of a variable?
+// // // just name of the variable and make it equal to whatever the new value is going to be
 
-// // How do we assign an existing variable to a new variable?
-// // ex. let newVar = oldVar;
+// // // How do we assign an existing variable to a new variable?
+// // // ex. let newVar = oldVar;
 
-// // Remind me, what are declare, assign, and define?
-// // declare is when you declare the variable, so assign name, not necessary to assgin it something
-// // assign is when you assign a specific value to the variable
-// // define is when you decalare and at the same time define what its going to hold
-
-
-// // What is pseudocoding and why should you do it?
-// //pseduocoding is when you type out the code in plain english to understand what needs to coded
-
-// // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
-
-// // most of the time you need to spend pseudocoding and understanding the logic behind the problem
-// // once you figured out the logic and the pseudocode then you need to start working on the code
-
-// // PART B
-
-// var firstVariable = "Hello World";
-// firstVariable = 12;
-// var secondVariable = firstVariable;
-// secondVariable = "Bye World";
-// console.log(firstVariable); // value of the first variable
-
-// var yourName = "Hrak T";
-// var newStr = "Hello my name is " + yourName;
-// console.log(newStr);
+// // // Remind me, what are declare, assign, and define?
+// // // declare is when you declare the variable, so assign name, not necessary to assgin it something
+// // // assign is when you assign a specific value to the variable
+// // // define is when you decalare and at the same time define what its going to hold
 
 
-// // PART C
+// // // What is pseudocoding and why should you do it?
+// // //pseduocoding is when you type out the code in plain english to understand what needs to coded
 
-// const a = 4;
-// const b = 53;
-// const c = 57;
-// const d = 16; 
-// const e = 'Kevin';
-// console.log(a != b);
-// console.log(c != d);
-// console.log('Name' == 'Name');
-// console.log(true != false);
-// console.log(false == false == false == false == false == false == true);
-// console.log(false == false)
-// console.log(e == 'Kevin');
-// console.log(a != b != c);
-// console.log(a == a != d);
-// console.log(48 == '48');
+// // // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+// // // most of the time you need to spend pseudocoding and understanding the logic behind the problem
+// // // once you figured out the logic and the pseudocode then you need to start working on the code
+
+// // // PART B
+
+// // var firstVariable = "Hello World";
+// // firstVariable = 12;
+// // var secondVariable = firstVariable;
+// // secondVariable = "Bye World";
+// // console.log(firstVariable); // value of the first variable
+
+// // var yourName = "Hrak T";
+// // var newStr = "Hello my name is " + yourName;
+// // console.log(newStr);
 
 
-// // PART D
+// // // PART C
 
-// var animal = 'cow';
-// if(animal = 'cow'){
-//     console.log('mooooo');
-// }else{
-//     console.log('hey you are not a cow');
-// }
+// // const a = 4;
+// // const b = 53;
+// // const c = 57;
+// // const d = 16; 
+// // const e = 'Kevin';
+// // console.log(a != b);
+// // console.log(c != d);
+// // console.log('Name' == 'Name');
+// // console.log(true != false);
+// // console.log(false == false == false == false == false == false == true);
+// // console.log(false == false)
+// // console.log(e == 'Kevin');
+// // console.log(a != b != c);
+// // console.log(a == a != d);
+// // console.log(48 == '48');
 
-// //Part E
 
-// var age = 26;
-// if(age>16){
-//     console.log("Here are the keys");
-// }else{
-//     console.log("Sorry youre too young");
-// }
+// // // PART D
+
+// // var animal = 'cow';
+// // if(animal = 'cow'){
+// //     console.log('mooooo');
+// // }else{
+// //     console.log('hey you are not a cow');
+// // }
+
+// // //Part E
+
+// // var age = 26;
+// // if(age>16){
+// //     console.log("Here are the keys");
+// // }else{
+// //     console.log("Sorry youre too young");
+// // }
 
  
-// II. Loops
+// // II. Loops
 
-// A. The basics
+// // A. The basics
 
-for(let i=0;i<=10;i++){
-    console.log(i);
-}
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
 
-for(let i = 10; i<=400;i++){
-    console.log(i);
-}
+// for(let i = 10; i<=400;i++){
+//     console.log(i);
+// }
 
-for(let i = 12; i<=4000;i+=3){
-    console.log(i);
-}
+// for(let i = 12; i<=4000;i+=3){
+//     console.log(i);
+// }
 
-// B. Get even
+// // B. Get even
 
 
-for(let i = 1;i<=100;i++){
-    if(i%2==0){
-        console.log(i + "<-- this is a even number");
+// for(let i = 1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i + "<-- this is a even number");
         
-    }else{
-        console.log(i);
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// C. Give me Five
+
+for(let i=0;i<=100;i++){
+    if(i%5==0 && i!=0){
+        console.log("I found a "+ i + ". High five!");
+    }
+    if(i%3 == 0 && i!=0){
+        console.log("I found a " + i+ ". There is a crowd.");
     }
 }
+
 
